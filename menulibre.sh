e55c3db4b8705f4f4f2d9c7e6e9b772889627029
TheEvilSkeleton <theevilskeleton@riseup.net>
2020-12-25 12:34:31 -0500

    Half support flatpaks

--- menulibre.sh
+++ menulibre.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:/run/host/usr/bin
-export XDG_DATA_DIRS=$XDG_DATA_DIRS:/run/host/usr/share
+export PATH=$PATH:/run/host/usr/bin:$HOME/.local/share/flatpak/exports/bin
+export XDG_DATA_DIRS=$XDG_DATA_DIRS:/run/host/usr/share:$HOME/.local/share/flatpak/exports/share
 exec /app/bin/menulibre
