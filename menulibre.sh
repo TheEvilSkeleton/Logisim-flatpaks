61de6c594353c9c0ac7f824e9fda8c521b59e80b
TheEvilSkeleton <theevilskeleton@riseup.net>
2020-12-26 14:55:03 -0500

    Automatically find host variables

--- menulibre.sh
+++ menulibre.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-export PATH=$PATH:/run/host/usr/bin:$HOME/.local/share/flatpak/exports/bin
-export XDG_DATA_DIRS=$XDG_DATA_DIRS:/run/host/usr/share:$HOME/.local/share/flatpak/exports/share
+NEWPATH="$(flatpak-spawn --host sh -c 'echo $PATH')"
+NEWXDGDATA="$(flatpak-spawn --host sh -c 'echo $XDG_DATA_DIRS')"
+IFS=:
+for x in $NEWPATH; do
+	case $x in
+		/home/*) export PATH="${x}:$PATH" ;;
+		*) export PATH="/run/host${x}:$PATH" ;;
+	esac
+done
+for x in $NEWXDGDATA; do
+	case $x in
+		/home/*) export XDG_DATA_DIRS="$x:$XDG_DATA_DIRS" ;;
+		*) export XDG_DATA_DIRS="/run/host${x}:$XDG_DATA_DIRS" ;;
+	esac
+done
+
+export PATH="$PATH:$HOME/.local/share/flatpak/exports/bin"
+export XDG_DATA_DIRS="$XDG_DATA_DIRS:$HOME/.local/share/flatpak/exports/share"
+
 exec /app/bin/menulibre
